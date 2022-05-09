e17cd87811ab320e6f7d6521bc5f0d9ed3c67a13
Mattias <mattias.mik@gmail.com>
2022-05-09 18:36:38 +0200

    Exposing baseURL for ensure public access

--- frontend/src/http-common.ts
+++ frontend/src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL,
+  baseURL: process.env.REACT_APP_SERVER_URL || 'http://localhost:5000/v1/api', // exposed only for public access reason
   headers: {
     "Content-type": "application/json"
   }
